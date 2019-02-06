b4fab9d15874fdb8f6021fb592e01a1002221ad2
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-02-06 17:54:34 -0600

    First push, added spotify api

---
+++ liri.js
@@ -0,0 +1,14 @@
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: "a765383554464e08be57bd565192bf5a",
+  secret: "5ab2b49008754a0fa79708411b90b9fc"
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
