d0a3b31ce6233d4d0aa7dd70a11d1624f85c8217
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-02-06 19:23:53 -0600

    Got spotify search to work

--- liri.js
+++ liri.js
@@ -12,14 +12,33 @@ var spotify = new Spotify({
   secret: "5ab2b49008754a0fa79708411b90b9fc"
 });
  
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
+spotify
+  .search({ type: 'track', query: 'All the Small Things' })
+  .then(function(response) {
+    console.log(response.tracks.items[0].album.name);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
+//   spotify
+//   .request('https://api.spotify.com/v1/search?q=tania%20bowra&type=artist')
+//   .then(function(data) {
+//     console.log(data.artists.items.id); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+//   });
 
+var input1 = process.argv[2];
+var input2 = process.argv[3];
 
-
+var spotifySearch = function () {
+    if (input1 = "spotify-this-song") {
+        spotify.search({type: 'track', query: input2})
+        console.log(data)
+    }
+    else {
+        console.log("Please enter in a valid command");
+    }
+}
