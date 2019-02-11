6c450f5d74e13ac4e50dba2b3dd1aa0af4016c8b
jeckhart582@gmail.com <jeckhart582@gmail.com>
2019-02-10 21:06:28 -0600

    Added ombd functionality

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # liri-node-app
 LIRI App
+
+![Spotify Seach](/Images/spotify.png)
+Format: ![Alt Text](url)
--- liri.js
+++ liri.js
@@ -4,41 +4,58 @@ require('dotenv').config()
 var keys = require("./keys");
 var axios = require("axios");
 var moment = require('moment');
+var bandsintown = require('bandsintown');
+var omdb = require('omdb');
 
 var Spotify = require('node-spotify-api');
- 
+
 var spotify = new Spotify({
   id: "a765383554464e08be57bd565192bf5a",
   secret: "5ab2b49008754a0fa79708411b90b9fc"
 });
- 
+
 spotify
   .search({ type: 'track', query: 'All the Small Things' })
-  .then(function(response) {
-    console.log(response.tracks.items[0].album.name);
+  .then(function (response) {
   })
-  .catch(function(err) {
+  .catch(function (err) {
     console.log(err);
   });
 
-//   spotify
-//   .request('https://api.spotify.com/v1/search?q=tania%20bowra&type=artist')
-//   .then(function(data) {
-//     console.log(data.artists.items.id); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
-
 var input1 = process.argv[2];
-var input2 = process.argv[3];
+var input2 = process.argv.slice(3).join(" ");
 
 var spotifySearch = function () {
-    if (input1 = "spotify-this-song") {
-        spotify.search({type: 'track', query: input2})
-        console.log(data)
-    }
-    else {
-        console.log("Please enter in a valid command");
-    }
+  if (input1 === "spotify-this-song") {
+    spotify.search({ type: 'track', query: input2 })
+      .then(function (response) {
+        var artist = response.tracks.items[0];
+        console.log("\n" + "-----------------------------------" + "\n" + "Artist: " + artist.artists[0].name + "\n" + "Song Name: " + input2 + "\n" + "Preview (link): " + artist.href + "\n" + "Album: " + artist.name + "\n");
+      });
+  }
+};
+
+var omdbSearch = function () {
+  if (input1 === "movie-this") {
+
+    var URL = "http://www.omdbapi.com/?t=" + input2 + "&apikey=142f2014";
+
+    axios.get(URL).then(
+      function (response) {
+        var movie = response.data
+        console.log("\n" + "-----------------------------------" + "\n" + "Artist: " + movie.Title+ "\n" + "Release Year: " + movie.Year + "\n" + "IMDB Rating: " + movie.imdbRating + "\n" + "Rotten Tomatoes Rating: " + movie.Metascore + "\n" + "Produced in :" + movie.Country + "\n" + "Language: " + movie.Language +  "\n" + "Plot Synopsis: " + movie.Plot + "\n" + "Actors: " + movie.Actors + "\n" );
+      });
+  }
 }
+
+// bandsintown
+//   .getArtistEventList('Skrillex')
+//   .then(function(events) {
+//     // return array of events
+//   });
+
+// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+spotifySearch();
+omdbSearch();
+
